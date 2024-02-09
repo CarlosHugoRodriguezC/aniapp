c563bc47d91991007f346b39f4ef631f7c05f637
CarlosHugoRodriguezC <cahuroca.aw@gmail.com>
2024-02-09 15:23:08 -0600

    Refactor media screen layout and add dynamic data

--- lib/presentation/screens/media/media_screen.dart
+++ lib/presentation/screens/media/media_screen.dart
@@ -15,8 +15,99 @@ class MediaScreen extends ConsumerWidget {
     final media$ = ref.watch(getMediaByIdProvider(int.parse(id)));
 
     return Scaffold(
-      body: media$.when(
-        data: (media) => _MediaScreenView(media),
+      body: CustomScrollView(
+        slivers: [
+          _AppBar(
+            media$: media$,
+          ),
+          _ViewContent(media$: media$),
+        ],
+      ),
+    );
+  }
+}
+
+class _AppBar extends StatelessWidget {
+  const _AppBar({
+    required this.media$,
+  });
+
+  final AsyncValue<MediaEntity> media$;
+
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme(:titleLarge, :titleSmall) = Theme.of(context).textTheme;
+
+    return SliverAppBar(
+      expandedHeight: 0.45.sh,
+      foregroundColor: Colors.white,
+      flexibleSpace: media$.when(
+        data: (media) => FlexibleSpaceBar(
+          collapseMode: CollapseMode.pin,
+          background: Stack(
+            children: [
+              Image.network(
+                media.bannerImage,
+                height: 0.35.sh,
+                fit: BoxFit.cover,
+              ),
+              Positioned(
+                child: Container(
+                  width: 1.sw,
+                  height: 0.35.sh,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      stops: const [0.05, 0.25, 0.95],
+                      colors: [
+                        Colors.black.withOpacity(0.5),
+                        Colors.transparent,
+                        Colors.black.withOpacity(0.7),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 0.01.sh,
+                right: 0.5.sw,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.all(Radius.circular(10.r)),
+                  child: Image.network(
+                    media.coverImage,
+                    height: 250.h,
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 0.20.sh,
+                right: 0.025.sw,
+                width: 0.45.sw,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      media.title,
+                      style: titleLarge?.copyWith(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w900,
+                        fontSize: 18.sp,
+                      ),
+                    ),
+                    Text(
+                      media.genres.join(', '),
+                      style: titleSmall?.copyWith(
+                        color: Colors.white70,
+                        fontSize: 14.sp,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
         loading: () => const Center(
           child: CircularProgressIndicator(),
         ),
@@ -27,66 +118,37 @@ class MediaScreen extends ConsumerWidget {
     );
   }
 }
+class _ViewContent extends StatelessWidget {
+  const _ViewContent({
+    required this.media$,
+  });
 
-class _MediaScreenView extends StatelessWidget {
-  const _MediaScreenView(this.media);
-
-  final MediaEntity media;
+  final AsyncValue<MediaEntity> media$;
 
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        SliverAppBar(
-          expandedHeight: 300,
-          foregroundColor: Colors.white,
-          flexibleSpace: FlexibleSpaceBar(
-            collapseMode: CollapseMode.parallax,
-            titlePadding: const EdgeInsets.symmetric(horizontal: 16.0),
-            title: Text(media.title),
-            background: Stack(
+    return SliverList(
+      delegate: SliverChildListDelegate([
+        media$.when(
+          data: (media) => Padding(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Image.network(
-                  media.coverImage,
-                  width: double.infinity,
-                  fit: BoxFit.cover,
-                ),
-                Container(
-                  decoration: const BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      stops: [0.0, 1.0],
-                      colors: [
-                        Colors.transparent,
-                        Colors.black,
-                      ],
-                    ),
-                  ),
+                Html(
+                  data: media.description,
                 ),
               ],
             ),
           ),
-        ),
-        SliverList(
-          delegate: SliverChildListDelegate(
-            [
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 8.h),
-                child: Text(
-                  media.genres.join(', '),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 8.w),
-                child: Html(
-                  data: media.description,
-                ),
-              ),
-            ],
+          loading: () => const Center(
+            child: CircularProgressIndicator(),
+          ),
+          error: (error, stack) => Center(
+            child: Text('Error: $error'),
           ),
         ),
-      ],
+      ]),
     );
   }
 }
